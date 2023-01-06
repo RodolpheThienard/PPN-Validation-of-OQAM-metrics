9b0d86e3249c7bede03173058ebd46a482fac7b3
corentinbeaulieu <corentin.beaulieu@ens.uvsq.fr>
2023-01-06 22:34:24 +0100

    time: fix a few typos
    The printing in conversion  was false due to a typo.
    The measure printed of the time taken by the loops is now correct.

--- test/time/conversion.c
+++ test/time/conversion.c
@@ -6,11 +6,11 @@
 
 // La taille peut être changé en fonction de l'architecture.
 // Elle doit être assez grande pour contrer la granularité du chronomètre.
-#define VEC_SIZE (1028 * 1028)
+#define VEC_SIZE (1028 *1028 * 1028)
 
 // Le nombre de répétitions des conversions.
 // Il doit être changé pour permettre à MAQAO d'avoir assez d'échantillons.
-#define NB_REP 20
+#define NB_REP 40
 
 // On souhaite un avertissement pour ces conversions (instructions cheres)
 void
@@ -39,9 +39,8 @@ int
 main (void)
 {
 
-    const size_t size = 1028 * 1028;
-    int *a = aligned_alloc (64, size * sizeof (int));
-    double *b = aligned_alloc (64, size * sizeof (double));
+    int *a = aligned_alloc (64, VEC_SIZE * sizeof (int));
+    double *b = aligned_alloc (64, VEC_SIZE * sizeof (double));
 
     struct timespec before, after;
     memset (&before, 0, sizeof (struct timespec));
@@ -54,10 +53,10 @@ main (void)
     for (size_t i = 0; i < NB_REP; i++)
         {
             clock_gettime (CLOCK_MONOTONIC_RAW, &before);
-            From_Int_To_Double (a, b, size);
+            From_Int_To_Double (a, b, VEC_SIZE);
             clock_gettime (CLOCK_MONOTONIC_RAW, &after);
             intToDoubleElapsed
-                += (double)(after.tv_sec - before.tv_sec) * 100000000
+                += (double)(after.tv_sec - before.tv_sec) * 1000000000
                    + (double)(after.tv_nsec - before.tv_nsec);
         }
 
@@ -65,10 +64,10 @@ main (void)
     for (size_t i = 0; i < NB_REP; i++)
         {
             clock_gettime (CLOCK_MONOTONIC_RAW, &before);
-            From_Double_To_Int (a, b, size);
+            From_Double_To_Int (a, b, VEC_SIZE);
             clock_gettime (CLOCK_MONOTONIC_RAW, &after);
             doubleToIntElapsed
-                += (double)(after.tv_sec - before.tv_sec) * 100000000
+                += (double)(after.tv_sec - before.tv_sec) * 1000000000
                    + (double)(after.tv_nsec - before.tv_nsec);
         }
 
@@ -83,7 +82,7 @@ main (void)
 
     // Ce dernier printf est obligatoire pour que le compilateur ne considère
     // pas les appels aux fonctions comme du dead code
-    printf ("%d %f\n", a[0], b[size - 1]);
+    printf ("%d %f\n", a[0], b[VEC_SIZE - 1]);
 
     free (a);
     free (b);
