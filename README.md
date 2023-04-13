b39faabba57438e3e8c24db175e677b5dc976423
Corentin Beaulieu <corentin.beaulieu@ens.uvsq.fr>
2023-04-13 14:45:30 +0200

    Modify: enhance the main README.md
    The general README.md is now useful. It gives information on the project
    and the tool.
    
    The reduc barrier code has been formated to follow the rest of the
    repository format.

--- README.md
+++ README.md
@@ -1,6 +1,49 @@
-# Validation des métriques de MAQAO et analyse critique de ses rapports
+# Validation des métriques de MAQAO 
+
+## INTRODUCTION
+
+[MAQAO](http://maqao.org/) (Modular Assembly Quality Analyzer and Optimizer) est un outil d'analyse statique et dynamique de code. 
+Il travaille au niveau du binaire pour aider les développeurs à améliorer les performances de leur application.
+
+Pour cela, il collecte des données sur le binaire analysé et l'exécution de ce dernier.
+Le but de ce projet scolaire est de valider les mesures et conclusions faites par MAQAO.
+
+Ce dépôt contient des benchmarks permettant de tester les différentes fonctionnalités de MAQAO.
+
+## USAGE
+
+Les benchmarks sont séparés selon si ils sont séquentiels ou parallèles (afin de correspondre aux objectifs des différents semestres).
+Chaque dossier contient le code source de benchmarks qui peut être compilé en utilisant la commande
+
+```bash
+$ make
+```
+
+La manière principale que nous avons utilisé pour faire une analyse avec MAQAO est grâce à la commande
+
+```bash
+$ maqao oneview -R1 -- <executable>
+```
+
+Certains tests nécessitent l'utilisation d'un fichier de configuration qui peut être passé à l'outil en utilisant 
+
+```bash
+$ maqao oneview -R1 -c=<config-file>
+```
+
+Pour plus de détails sur l'utilisation de MAQAO, vous pouvez vous rediriger vers le site [](http://maqao.org/) ou les pages de manuel de celui-ci.
+
+Pour plus de détails sur les différents codes, un `README.md` se trouve dans chaque dossier.
+
+## CRÉDITS
+
+MAQAO est développé par **MAQAO team**.
+
+Notre projet de validation est encadré par **Emmanuel Oseret** et **Cédric Valensi** de l'équipe MAQAO.
+
+## AUTEURS
 
 - Corentin BEAULIEU
+- Zakaria  EJJED
 - Ny Aina  PEDERSEN
 - Rodolphe THIENARD
-- Zakaria  EJJED
--- parallel/load-balancing/reduc_first_all_lb_barrier.c
+++ parallel/load-balancing/reduc_first_all_lb_barrier.c
@@ -1,31 +1,33 @@
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
 #include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 // The vector should fit in the L1 cache
-#define SIZE 1024*16
-
+#define SIZE 1024 * 16
 
 pthread_barrier_t barrier;
 
 // Naive reduction of a vector
-static double reduc (const double *restrict tab, const size_t size) {
+static double
+reduc (const double *restrict tab, const size_t size)
+{
 
     double res = 0.0F;
 
-    for(size_t i = 0; i < size; i++) {
+    for (size_t i = 0; i < size; i++)
+        {
 
-        res -= tab[i];
-    }
+            res -= tab[i];
+        }
 
     return res;
 }
 
-
 // Structre and routine to fill the vector
-typedef struct {
+typedef struct
+{
 
     double *tab;
     size_t size;
@@ -33,21 +35,24 @@ typedef struct {
 
 } FillingArgs;
 
-void * filling_routine (void *args) {
+void *
+filling_routine (void *args)
+{
 
-    FillingArgs *filling = (FillingArgs*) args;
+    FillingArgs *filling = (FillingArgs *)args;
 
-    for(size_t i = 0; i < filling->size; i++) {
+    for (size_t i = 0; i < filling->size; i++)
+        {
 
-        filling->tab[i] = 0.1F * (i + filling->first); 
-    }
+            filling->tab[i] = 0.1F * (i + filling->first);
+        }
 
     return NULL;
 }
 
-
 // Structure and routine to reduce the vector
-typedef struct {
+typedef struct
+{
 
     double *tab;
     double ret;
@@ -56,111 +61,129 @@ typedef struct {
 
 } ComputeArgs;
 
-void * compute_routine (void *args) {
+void *
+compute_routine (void *args)
+{
 
-    pthread_barrier_wait(&barrier);
+    pthread_barrier_wait (&barrier);
 
-    ComputeArgs *compute = (ComputeArgs *) args;
+    ComputeArgs *compute = (ComputeArgs *)args;
 
     struct timespec begin, end;
-    clock_gettime(CLOCK_MONOTONIC_RAW, &begin);
+    clock_gettime (CLOCK_MONOTONIC_RAW, &begin);
 
     compute->ret = 0.0F;
 
-    for(size_t i = 0; i < compute->nbIter; i++) {
+    for (size_t i = 0; i < compute->nbIter; i++)
+        {
 
-        compute->ret += reduc(compute->tab, SIZE); 
-    }
+            compute->ret += reduc (compute->tab, SIZE);
+        }
 
-    pthread_barrier_wait(&barrier);
+    pthread_barrier_wait (&barrier);
 
-    clock_gettime(CLOCK_MONOTONIC_RAW, &end);
-    compute->elapsed = (double) (end.tv_sec - begin.tv_sec) + (double) (end.tv_nsec - begin.tv_nsec) * 1e-9;
+    clock_gettime (CLOCK_MONOTONIC_RAW, &end);
+    compute->elapsed = (double)(end.tv_sec - begin.tv_sec)
+                       + (double)(end.tv_nsec - begin.tv_nsec) * 1e-9;
 
     return NULL;
 }
 
-
-int main (int argc, char *argv[]) {
+int
+main (int argc, char *argv[])
+{
 
     struct timespec begin, end;
 
-    if(argc != 4)
-        return fprintf(stderr, "USAGE: %s nb_iteration nb_threads nb_chunks\n", argv[0]), 1;
+    if (argc != 4)
+        return fprintf (stderr,
+                        "USAGE: %s nb_iteration nb_threads nb_chunks\n",
+                        argv[0]),
+               1;
 
-    clock_gettime(CLOCK_MONOTONIC_RAW, &begin);
+    clock_gettime (CLOCK_MONOTONIC_RAW, &begin);
 
-    const size_t nbIter  = strtoul(argv[1], NULL, 10);
-    const size_t nbThrd  = strtoul(argv[2], NULL, 10);
-    const size_t nbChunk = strtoul(argv[3], NULL, 10);
+    const size_t nbIter = strtoul (argv[1], NULL, 10);
+    const size_t nbThrd = strtoul (argv[2], NULL, 10);
+    const size_t nbChunk = strtoul (argv[3], NULL, 10);
 
-    double *tab = (double *) aligned_alloc(32, SIZE * sizeof(double));
-    pthread_t thrdID[nbThrd]; 
-    FillingArgs *filling = aligned_alloc(32, nbThrd * sizeof(FillingArgs));
+    double *tab = (double *)aligned_alloc (32, SIZE * sizeof (double));
+    pthread_t thrdID[nbThrd];
+    FillingArgs *filling = aligned_alloc (32, nbThrd * sizeof (FillingArgs));
 
-    pthread_barrier_init(&barrier, NULL, nbThrd);
+    pthread_barrier_init (&barrier, NULL, nbThrd);
 
-    for(size_t i = 0; i < nbThrd; i++) {
+    for (size_t i = 0; i < nbThrd; i++)
+        {
 
-        if(i == 0) {
+            if (i == 0)
+                {
 
-            filling[i].size = (SIZE - nbThrd + 1);
-            filling[i].tab = tab;
-            filling[i].first = i * (SIZE / nbThrd);
+                    filling[i].size = (SIZE - nbThrd + 1);
+                    filling[i].tab = tab;
+                    filling[i].first = i * (SIZE / nbThrd);
+                }
+            else
+                {
+
+                    filling[i].size = 1;
+                    filling[i].tab = &(tab[SIZE - nbThrd + i]);
+                    filling[i].first = SIZE - nbThrd + i;
+                }
+
+            pthread_create (&thrdID[i], NULL, filling_routine, &filling[i]);
         }
-        else {
 
-            filling[i].size = 1;
-            filling[i].tab = &( tab[SIZE - nbThrd + i] );
-            filling[i].first = SIZE - nbThrd + i;
+    for (size_t i = 0; i < nbThrd; i++)
+        {
+
+            pthread_join (thrdID[i], NULL);
         }
 
-        pthread_create(&thrdID[i], NULL, filling_routine, &filling[i]);
-    }
-
-    for(size_t i = 0; i < nbThrd; i++) {
-
-        pthread_join(thrdID[i], NULL);
-    }
-
-    free(filling);
+    free (filling);
 
     double res = 0.0F;
-    ComputeArgs *compute = aligned_alloc(32, nbThrd * sizeof(ComputeArgs));
+    ComputeArgs *compute = aligned_alloc (32, nbThrd * sizeof (ComputeArgs));
     size_t chunk = nbIter / nbChunk;
 
-    for(size_t i = 0; i < nbThrd; i++) {
+    for (size_t i = 0; i < nbThrd; i++)
+        {
 
-        if(i == 0) {
+            if (i == 0)
+                {
 
-            compute[i].nbIter = (nbChunk - nbThrd + 1) * chunk + chunk % nbChunk; 
+                    compute[i].nbIter
+                        = (nbChunk - nbThrd + 1) * chunk + chunk % nbChunk;
+                }
+            else
+                {
+
+                    compute[i].nbIter = chunk;
+                }
+
+            compute[i].tab = tab;
+            compute[i].ret = 0.0F;
+
+            pthread_create (&thrdID[i], NULL, compute_routine, &compute[i]);
         }
-        else {
 
-            compute[i].nbIter = chunk;
+    for (size_t i = 0; i < nbThrd; i++)
+        {
+
+            pthread_join (thrdID[i], NULL);
+            res += compute[i].ret;
+            fprintf (stderr, "%3.9lf\n", compute[i].elapsed);
         }
 
-        compute[i].tab = tab;
-        compute[i].ret = 0.0F;
+    free (compute);
 
-        pthread_create(&thrdID[i], NULL, compute_routine, &compute[i]);
-    }
+    printf ("%lf\n", res);
 
-    for(size_t i = 0; i < nbThrd; i++) {
+    clock_gettime (CLOCK_MONOTONIC_RAW, &end);
 
-        pthread_join(thrdID[i], NULL);
-        res += compute[i].ret;
-        fprintf(stderr, "%3.9lf\n", compute[i].elapsed); 
-    }
-
-    free(compute);
-
-    printf("%lf\n", res);
-
-    clock_gettime(CLOCK_MONOTONIC_RAW, &end);
-
-    double elapsed = (double) (end.tv_sec - begin.tv_sec) + (double) (end.tv_nsec - begin.tv_nsec) * 1e-9;
-    fprintf(stderr, "%3.9lf\n", elapsed); 
+    double elapsed = (double)(end.tv_sec - begin.tv_sec)
+                     + (double)(end.tv_nsec - begin.tv_nsec) * 1e-9;
+    fprintf (stderr, "%3.9lf\n", elapsed);
 
     return 0;
 }
